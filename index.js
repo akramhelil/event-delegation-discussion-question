b6a28d5dbd2ca286792044f1cec966b092aabee0
akramhelil <akramhelil@gmail.com>
2019-04-05 11:49:31 -0400

    Finished

--- index.js
+++ index.js
@@ -1,5 +1,30 @@
 document.addEventListener('DOMContentLoaded', () => {
   const catContainer = document.getElementById('cat-container')
+  const header = document.querySelector('#cat-header')
+  const catHeader = document.querySelectorAll('.ui.header')
+  
+
+    header.addEventListener('click', ev => {
+      ev.target.className = 'ui blue header center aligned'
+  })
+    // catHeader.map((cat) => {
+    //   cat
+    // })
+    catContainer.addEventListener('click', ev => {
+      const clicked = ev.target 
+      if (clicked.matches('.ui.header')){
+        clicked.className = 'ui header green'
+      }else if (clicked.dataset.action === 'like'){
+          const id = parseInt(clicked.dataset.id)
+          let likeCount = document.getElementById(`cat-${id}`)
+          let countNum = parseInt(likeCount.innerText)
+            countNum += 1 
+          likeCount.innerText = countNum 
+          console.log(countNum)
+        }
+    })
+
+   
 
   catContainer.innerHTML = renderCats()
 
@@ -23,6 +48,7 @@ function renderOneCat(cat) {
               </a>
             </div>
           </div>`
+          
 }
 
 function renderCats() {
